69ecec898d7c6bdc732ea91e9d65d37f95f1979c
sanjay <sanjayprashanth1997@gmail.com>
2023-03-10 10:08:16 +0530

    commit_new_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const data = [
+    {
+      productImage:
+        "https://www.apple.com/in/iphone/home/images/meta/iphone__ky2k6x5u6vue_og.png",
+      productName: "Iphone 14promax",
+      price: 100000,
+      rating: 91,
+    },
+    {
+      productImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3jAmZcKm3NQoK8tlikVJsPAfm3cAhr_IKelWibDsJj3vgZ5BCZuvssOPGjIbljbkWR68&usqp=CAU",
+      productName: "Samsung Galaxy",
+      price: 95000,
+      rating: 90,
+    },
+    {
+      productImage:
+        "https://images.officeworks.com.au/api/2/img/https://s3-ap-southeast-2.amazonaws.com/wc-prod-pim/JPEG_1000x1000/GPX7PR128H_A_google_pixel_7_pro_5g_unlocked_smartphone_128gb_hazel.jpg/resize?size=600&auth=MjA5OTcwODkwMg__",
+      productName: "Google Pixel 7pro",
+      price: 70000,
+      rating: 80,
+    },
+    {
+      productImage:
+        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/617MPEZB5mL._SL1500_.jpg",
+      productName: "Oneplus 11",
+      price: 64000,
+      rating: 50,
+    },
+  ];
+
+  
+  const show = true
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-container">
+      {data.map((prod, idx) => (
+        <Card key = {idx} style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={prod.productImage} />
+          <Card.Body>
+            <Card.Title>{prod.productName}</Card.Title>
+            <Card.Text>{prod.price} RS</Card.Text>
+            <Card.Text>{prod.rating} ♥</Card.Text>
+                        {/* conditional rendering */}
+
+            <Button
+             variant="primary"
+             onClick={()=>console.log("buy clicked")}
+             >Buy Now</Button>
+
+
+             {show ? <Button 
+             variant="danger"
+             onClick={()=>console.log("remove clicked")}
+             >remove now</Button> : ""}
+
+
+          </Card.Body>
+        </Card>
+      ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// Working with array lists
+// Usage of keys
+// Conditional rendering
+// Handling Events
+// Props vs state
